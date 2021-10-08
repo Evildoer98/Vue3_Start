5cb8c007c25b58093daeed1ad8f64d4abb026d28
徐世成 <1213089093@qq.com>
2021-10-08 11:43:14 +0800

    router and store

--- demo/vite-project/src/main.ts
+++ demo/vite-project/src/main.ts
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+// import router from './router/index'
+// import store from './store/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+
+
+// createApp(App).mount('#app')
+// createApp(App).use(router).mount('#app')
+// createApp(App).use(store).mount('#app')
+createApp(App).use(ElementPlus).mount('#app')
+
+
+
+
