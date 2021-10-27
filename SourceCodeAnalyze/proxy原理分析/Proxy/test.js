469cff1c1b9beb4b80b6ed5ab9530ae9163ebb9c
徐世成 <1213089093@qq.com>
2021-10-27 23:49:32 +0800

    feat: 2021/10/27 SourceCodeAnalyze-proxy原理分析-Proxy-test.js

--- SourceCodeAnalyze/proxy原理分析/Proxy/test.js
+++ SourceCodeAnalyze/proxy原理分析/Proxy/test.js
@@ -4,7 +4,7 @@
  * @Author: Evildoer98
  * @Date: 2021-10-24 19:04:58
  * @LastEditors: Evildoer98
- * @LastEditTime: 2021-10-24 22:49:30
+ * @LastEditTime: 2021-10-27 23:48:13
  */
 
 /**
@@ -27,17 +27,32 @@
 // 自定义对象属性的获取、赋值、枚举、函数调用的等功能
 
 var target = {
-    a: 3,
+    a: 1,
     b: 2
 }
 let proxy = new Proxy(target, {
     get (target, prop) {
         console.log('this is property value ' + target[prop]);
+        return 'this is property value' + target[prop]
     },
-    set () {
-
+    set (target, prop, value) {
+        target[prop] = value
+        console.log(target[prop]);
     }
 })
 
 console.log(proxy.a);
+console.log(target.a);
+proxy.b = 3
+console.log(proxy);
+console.log(target);
 
+
+let arr = [
+  {name: '小明', age: 18},
+  {name: '小红', age: 28},
+  {name: '小青', age: 38},
+  {name: '小橙', age: 48},
+  {name: '小黄', age: 58},
+  {name: '小绿', age: 68}
+]
